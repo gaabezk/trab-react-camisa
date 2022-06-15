24e87433826315b6188fee46f2eb2361a12cda73
feittosaa <lucas.feitosa2004@gmail.com>
2022-06-15 17:57:18 -0300

    .

--- src/components/NavBar2.jsx
+++ src/components/NavBar2.jsx
@@ -8,7 +8,7 @@ export function NavBar2() {
                 <div className="container noPading">
                     <ul className="nav">
                         <li className="nav-item">
-                            <img src="https://cdn-icons-png.flaticon.com/512/103/103972.png" alt="www"/>
+                            <img src="https://media.discordapp.net/attachments/953010683455357009/986735200820150322/camisa_10.png" alt="www"/>
                         </li>
                         <li className="nav-item">
                             <a href="http://localhost:3000/">Home</a>
