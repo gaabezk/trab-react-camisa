2f533c828c4984677e8447904038ff6c3c325a9b
Gabriel Fernandes <gaabezk@gmail.com>
2022-06-15 19:46:06 -0300

    fix

--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -4,7 +4,8 @@ export function Catalogo() {
 
     return (
         <>
-            <img width="100%" src="https://www.rioprevidencia.rj.gov.br/cs/groups/public/documents/imagem/cnbf/mdix/~edisp/rp_021445.jpg"/>
+            <img width="100%"
+                 src="https://www.rioprevidencia.rj.gov.br/cs/groups/public/documents/imagem/cnbf/mdix/~edisp/rp_021445.jpg"/>
 
             <div className='row'>
                 <div className="col-lg-4 col-md-4 col-sm-4">
@@ -24,7 +25,8 @@ export function Catalogo() {
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-4">
                     <Produto nome="CAMISA" img="/src/img/camiseta.png"/>
-                </div><div className="col-lg-4 col-md-4 col-sm-4">
+                </div>
+                <div className="col-lg-4 col-md-4 col-sm-4">
                     <Produto nome="CAMISA" img="/src/img/camiseta.png"/>
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-4">
