b1c42e48900d9cc907880731ff7711f206bb7421
RenanLopes1 <renannfl1@gmail.com>
2022-06-21 17:34:09 -0300

    home pronta

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import '../style/Home.css'
 
+<<<<<<< Updated upstream
 
 export function Home(){
     return (
@@ -8,6 +9,26 @@ export function Home(){
             <h1>CAMISA<span>10</span></h1>
             <p>A camisa do seu time de coração!</p>
         </section>
+=======
+export function Home(){
+    return (
+
+        <section className="container">
+           <h1>Camisa<span>10</span></h1> 
+           <img src="/src/img/futebol.webp" alt="Football"></img>
+           <h2>A camisa do seu time de coração!</h2>
+           <p>Os melhores times do Rio de Janeiro</p>
+            
+            <div>
+            <img src="/src/img/fluminense.png" alt="Fluminense"></img>
+            <img src="/src/img/flamengo.png" alt="Flamengo"></img>  
+            <img src="/src/img/vasco.png" alt="Vasco"></img>
+            <img src="/src/img/botafogo.png" alt="Botafogo"></img>
+            </div>
+            
+        </section>
+
+>>>>>>> Stashed changes
     )
 }
 
