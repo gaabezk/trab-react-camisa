66c2008b9c0dfacfec65fa42f83a81dadedde7af
HeitorCalmeida <heitorcalmeida@gmail.com>
2022-06-21 17:50:01 -0300

    get usuario comentado

--- src/pages/Usuario.jsx
+++ src/pages/Usuario.jsx
@@ -1,8 +1,31 @@
 import InserirEndereco from "../components/InserirEndereco";
 import UserNewAccount from "../components/UserNewAccount";
+import { CardUsuario } from "../components/CardUsuario";
+import API from "../API";
+import { useEffect, useState } from "react";
 import '../style/Cadastro.css'
 
-export function Usuario() {
+export const Usuario = () => {
+
+    // const [cliente, setCliente] = useState([]);
+
+    // useEffect(() => {
+    //     const pegarDados = async () => {
+    //         const resposta = await API.get("/usuario/cliente/22506140085");
+    //         setCliente(resposta.data);
+    //         console.log(resposta.data)
+    //     }
+
+    //     pegarDados();
+    // }, []);
+
+    // const novoArray = cliente.map((t, index) => {
+    //     return (
+    //         <div key={index} className="col-xl-3 col-lg-4 col-md-4 col-sm-6">
+    //             <CardUsuario nome={t.nome} cpf={t.cpf} telefone={t.telefone} dataNascimento={t.dataNascimento} email={t.email} userName={t.userName} />
+    //         </div>
+    //     )
+    // })
 
     return (
         <>
@@ -13,6 +36,9 @@ export function Usuario() {
                 <div className="col-lg-4 col-md-4 col-sm-4">
                     <InserirEndereco />
                 </div>
+                {/* <div className="col-lg-4 col-md-4 col-sm-4">
+                    {novoArray}
+                </div> */}
             </div>
             <br />
             <br />
